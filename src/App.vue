3d9e84cbe277de3b9fc435ca2ebeb25c0ea52fca
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-02-20 21:35:49 +0300

    Added Navbar

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue';
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <navbar msg="sadaf"></navbar>
+</template>
+
+<style>
+@import url(https://gist.githubusercontent.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/2923dc0cccd912c217ef244f5c42d547deab5b60/Gilroy.css);
+#app {
+  font-family: Gilroy,Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="w-full h-[130px] bg-black content-center">
+    <img src="../assets/logo.png" class="inline-block w-1/5 ">
+    <div class="inline-flex justify-around w-4/5 h-full align-middle ">
+      <a class="mt-auto mb-auto text-[30px] hover:underline">Профиль</a>
+      <a class="mt-auto mb-auto text-[30px] hover:underline">Организации</a>
+      <a class="mt-auto mb-auto text-[30px] hover:underline">Сообщения</a>
+      <a class="mt-auto mb-auto text-[30px] hover:underline">Настройки</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: white;
+}
+</style>
